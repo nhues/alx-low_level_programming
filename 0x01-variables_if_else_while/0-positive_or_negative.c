9de64362b985ab2a3d6941907f6ee6323fb9f6e6
nhues <94398444+nhues@users.noreply.github.com>
2022-03-14 20:33:35 +0000

    task 0 variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,24 +6,24 @@
  *
  * Return: Always 0 (Success/correct)
  */
-int main(void)
-		{
-		int n;
-		srand(time(0));
-		n=rand() -  RAND_MAX / 2;
+int main(void)	
+{
+int n;
+srand(time(0));
+n=rand() -  RAND_MAX / 2;
  
-		if (n==0)
-		{
-		printf("%i is a zero\n", n);
-		}
-		else if (n<0)
-		{
-		printf("%i is negative\n", n);
-		}
-		else
-		{
-		printf("%i is positive\n", n);
-		}
-		return (0);
- 
-		}
+if (n==0)
+{
+printf("%i is a zero\n", n);
+}
+else if (n<0)
+{
+printf("%i is negative\n", n);
+}
+else
+{
+printf("%i is positive\n", n);
+}
+return (0);
+
+}
