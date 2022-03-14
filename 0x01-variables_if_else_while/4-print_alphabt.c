d1e57c379da8e7833f41a87fcab501032d04aa44
nhues <94398444+nhues@users.noreply.github.com>
2022-03-14 22:34:32 +0000

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,10 +5,11 @@
  * Return: Always 0 (Success/correct)
  */
 int main(void)
+
 {
 	char alphabet;
 
-	for (alphabet = 'a' alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		if (alphabet == 'q')
 			continue;
