aeb4a0ca76af611b157fe47b4941d5cd0a2cd6f2
nhues <94398444+nhues@users.noreply.github.com>
2022-03-17 21:40:44 +0000

    functions task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,39 +11,39 @@ void times_table(void)
 	int c;
 	for (a = 0; a <= 9; a++)
 	{
-		for (a =0; a <= 9; a++)
-		{
-			c = a* b;
-			if ((c / 10) == 0)
-			{
-				if (b ==0)
-				{
-					_putchar('0');
-				}
-				if (b != 0)
-				{
-					_puchar (' ');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				_putchar ((c / 10 + '0');
-				_putchar((c % 10) + '0');
-						if (b < 9)
-						{
-						_putchar(',');
-						_putchar(' ');
-						}
-						}
-						}
-						_putchar ('\n');
+	for (a = 0; a <= 9; a++)
+	{
+	c = a * b;
+	if ((c / 10) == 0)
+	{
+	if (b == 0)
+	{
+	_putchar ('0');
+	}
+	if (b != 0)
+	{
+	_putchar (' ');
+	_putchar ((c % 10) + '0');
+	}
+	if (b < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	else
+	{
+	_putchar ((c / 10 + '0');
+	_putchar((c % 10) + '0');
+	if (b < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	}
+	_putchar ('\n');
 
-						}
-						}
+	}
+        }
 
