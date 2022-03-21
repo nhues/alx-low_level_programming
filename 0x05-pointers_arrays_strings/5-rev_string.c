c2ddefca986ff0b8d98019d66bd38732130bdcfe
nhues <94398444+nhues@users.noreply.github.com>
2022-03-21 23:00:47 +0000

    task arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,30 @@
-#include "main.h"
+#include main.h
 
 /**
- *  * rev_string - reverse a string.
- *   *
- *    *@s: the string to be reversed.
- *     */
+ * rev_string - reverse a string.
+ *
+ *@s: the string to be reversed.
+ */
 
 void rev_string(char *s)
 {
-		int length, j, i;
-			char v1, v2;
+int length, j, i;
+char v1, v2;
 
-				for (length = 0; s[length] != '\0'; length++)
-						{
-								}
+for (length = 0; s[length] != '\0'; length++)
+{
+}
 
-					j = length - 1;
-						i = 0;
+j = length - 1;
+i = 0;
 
-							while (j > i)
-									{
-												v1 = s[i];
-														v2 = s[j];
-																s[i] = v2;
-																		s[j] = v1;
-																				j--;
-																						i++;
-																							}
+while (j > i)
+{
+v1 = s[i];
+v2 = s[j];
+s[i] = v2;
+s[j] = v1;
+j--;
+i++;
+}
 }
