8c5b234abc1248a93ff94cfbf8ecf46d63d611d3
nhues <94398444+nhues@users.noreply.github.com>
2022-03-25 21:49:04 +0000

    advance task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,7 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, op, bg, dr1 dr2, add = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1 dr2, add = 0;
 
 	while (*(n1 + c1) != '\0')
 		c1++;
@@ -25,7 +25,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
-	dr = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
@@ -38,7 +38,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			*(r + bg) = '0';
 		if (c1 > 0)
-			c1--, dr2 = *(n2 + c2) - 48;
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
 		bg--, size_r--;
