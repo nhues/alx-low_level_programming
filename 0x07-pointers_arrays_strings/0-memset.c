289b1e6086c33fbd5dec26c5056215c9303620af
nhues <94398444+nhues@users.noreply.github.com>
2022-03-31 20:22:15 +0000

    0x07 tasks

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,8 +3,8 @@
  *_memset -  fills memory with a constant byte 
  *@s: pointer block of memory to fill
  *@b: value to set
- *@n:  bytes of the memory
- *Return:  dest
+ *@n: bytes of the memory
+ *Return: dest
  */
  char *_memset(char *s, char b, unsigned int n)
 
